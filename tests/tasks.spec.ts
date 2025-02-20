7a6e7e93802f0c5a0585a2408d6fde6a8750f008
Eduardo Berg <duhberg@gmail.com>
2025-02-20 20:49:52 -0300

    Initial commit

---
+++ tests/tasks.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+import { TaskModel } from './fixtures/task.model';
+import { deleteTaskByHelper, postTask } from './support/helpers';
+import { TasksPage } from './support/pages/tasks';
+
+test('Deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
+
+    const task: TaskModel = {
+        name: 'Ler um livro de TypeScript',
+        is_done: false
+    }
+
+    await deleteTaskByHelper(request, task.name)
+
+    const tasksPage: TasksPage = new TasksPage(page)
+    await tasksPage.go()
+    await tasksPage.create(task)
+    await tasksPage.shouldHaveText(task.name)
+
+})
+
+
+test('Não deve permitir tarefa duplicada', async ({ page, request }) => {
+
+    const task: TaskModel = {
+        name: 'Comprar Ketchup',
+        is_done: false
+    }
+
+    await deleteTaskByHelper(request, task.name)
+    await postTask(request, task)
+
+    const tasksPage: TasksPage = new TasksPage(page)
+    await tasksPage.go()
+    await tasksPage.create(task)
+    await tasksPage.alertHaveText('Task already exists!')
+
+})
