3821b3e8d7ca50703b78883ccb9838a5f86872bb
Eduardo Berg <duhberg@gmail.com>
2025-02-20 22:18:02 -0300

    Alterado urls para arquivo .env

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
+require('dotenv').config()
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -31,7 +33,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: true,
-    baseURL: 'http://127.0.0.1:8080/'
+    baseURL: process.env.BASE_URL,
   },
 
   /* Configure projects for major browsers */
